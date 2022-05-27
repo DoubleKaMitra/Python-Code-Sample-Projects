450eba11eb0a02eeaa115fb57ddff094c2746a54
DoubleKaMitra <104017419+DoubleKaMitra@users.noreply.github.com>
2022-05-27 01:28:38 -0500

    Finding Weight Based on User Input
    Finding weight in Kg or Lbs based on the user selection.

---
+++ Weight_in_Kg_or_Lbs.py
@@ -0,0 +1,20 @@
+# Python Simple Weight Calculator in Kg or Lbs
+
+weight = int(input("Please enter your weight: "))
+count = 0
+count_max = 3
+
+while count < count_max:
+    k_or_l = input("(K)g or (L)bs: ")
+    count += 1
+    if k_or_l.upper() == 'L':
+        print(count)
+        weight = round((weight / 2.2), 2)
+        print("You're weight in kilograms is " + str(weight))
+        break
+    elif k_or_l.upper() == 'K':
+        weight = round((weight * 2.2), 2)
+        print("You're weight in pounds is " + str(weight))
+        break
+else:
+    print("Try again")
